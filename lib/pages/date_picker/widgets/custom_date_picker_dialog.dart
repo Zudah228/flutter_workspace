69e63543bcdb3715d2299dab92ecfc8dab594641
Zudah228 <rezel95.hin@gmail.com>
2022-07-11 16:05:08 +0900

    CustomDatePickerのUIを調節

--- lib/pages/date_picker/widgets/custom_date_picker_dialog.dart
+++ lib/pages/date_picker/widgets/custom_date_picker_dialog.dart
@@ -18,15 +18,16 @@ class CustomDatePickerDialog extends StatefulWidget {
       {required DateTime initialDate,
       required DateTime firstDate,
       required DateTime lastDate}) async {
-    final result = await showDialog<DateTime?>(
-        context: context,
-        builder: (context) {
-          return CustomDatePickerDialog(
-            initialDate: initialDate,
-            firstDate: firstDate,
-            lastDate: lastDate,
-          );
-        });
+    final result =
+        await Navigator.of(context).push<DateTime?>(DialogRoute<DateTime?>(
+            context: context,
+            builder: (context) {
+              return CustomDatePickerDialog(
+                initialDate: initialDate,
+                firstDate: firstDate,
+                lastDate: lastDate,
+              );
+            }));
     return result;
   }
 
@@ -45,37 +46,58 @@ class _CustomDatePickerDialogState extends State<CustomDatePickerDialog> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Dialog(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(date.format(pattern: 'yyyy M月dd日(E)', useJapaneseEra: true)),
-            CustomCalendarDatePicker(
-                initialDate: widget.initialDate,
-                firstDate: widget.firstDate,
-                lastDate: widget.lastDate,
-                onDateChanged: (value) {
-                  setState(() {
-                    date = value;
-                  });
-                }),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                    onPressed: Navigator.of(context).pop,
-                    child: const Text('キャンセル')),
-                TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop(date);
-                    },
-                    child: const Text('決定')),
-              ],
-            )
-          ],
-        ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: ColoredBox(
+                color: theme.appBarTheme.backgroundColor ??
+                    theme.primaryColor,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 32,
+                    left: 16,
+                    bottom: 16
+                  ),
+                  child: Text(
+                    date.format(
+                      pattern: 'yyyy M月dd日(E)', useJapaneseEra: true),
+
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                        color: theme.appBarTheme.titleTextStyle?.color ?? Colors.white
+                      ),
+                  ),
+                )),
+          ),
+          CustomCalendarDatePicker(
+              initialDate: widget.initialDate,
+              firstDate: widget.firstDate,
+              lastDate: widget.lastDate,
+              onDateChanged: (value) {
+                setState(() {
+                  date = value;
+                });
+              }),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                  onPressed: Navigator.of(context).pop,
+                  child: const Text('キャンセル')),
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(date);
+                  },
+                  child: const Text('決定')),
+            ],
+          )
+        ],
       ),
     );
   }
